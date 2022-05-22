742b76b0f9b9e74cd64fd7a360f399814109855d
moritz.steiner2@gcloud-vm <moritz.steiner2@uzh.ch>
2022-05-22 14:53:31 +0000

    Add files for evaluation in task 2

---
+++ scripts/bleu_beam_graph.py
@@ -0,0 +1,22 @@
+#! usr/bin/env python
+# script to visualize the impact of beam size on the bleu score
+ 
+import pandas as pd
+import argparse
+import matplotlib.pyplot as plt
+ 
+def cli():
+    parser = argparse.ArgumentParser("script to visualize the impact of beam size on the bleu score")
+    parser.add_argument("-i", help="input .csv with the beam size in the first and BLEU score in the second column")
+    parser.add_argument("-o", help="path to the .png the graph should be saved to")
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    df = pd.read_csv(args.i, names=["Beam size", "BLEU"])
+    line = df.plot.line(x="Beam size", y="BLEU")
+    #plt.show()
+    plt.savefig(args.o)
+
+main(cli())
+        
